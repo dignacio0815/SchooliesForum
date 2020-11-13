b2bdf1a989e32abb39c438336721716382e3bdd9
Jesus Caballero <voraphi@outlook.com>
2020-11-13 03:17:30 +0000

    sessions

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express')
 const path = require('path');
+const session = require('express-session');
 const app = express();
 let port = process.env.PORT || 3000;
 
 // Router Setup
 let indexRouter = require('./routes/index');
+let logigRouter = require('./routes/login');
+let accountRouter = require('./routes/account');
 
 app.use(express.static('public'));
 app.use('/css', express.static(__dirname + 'public/css'));
+app.use( session ({ secret: "Gimme some Cheese", resave: true, saveUninitialized: true }));
 
 // View engine set up
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
 app.use('/', indexRouter);
+app.use('/login', logigRouter);
+app.use('/account', accountRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
