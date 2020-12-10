5d8bc0224f50408bd90d543f02c3697232af2605
Alexis Caasi <alexiscaasi0@gmail.com>
2020-12-10 12:18:50 -0800

    user can logout

---
+++ routes/logout.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    req.session.destroy((err) =>{
+        res.redirect('/')
+    })
+});
+
+module.exports = router;
