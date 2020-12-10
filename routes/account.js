99b634e98eef14ec5cffb8e3bcb0929e74320842
Jesus Caballero <voraphi@outlook.com>
2020-12-10 19:06:17 +0000

    added university list in register

--- routes/account.js
+++ routes/account.js
@@ -3,62 +3,69 @@ const router = express.Router();
 
 router.get('/', isAuthenticated, async (req, res) => {
     
-    let stmt = 'select * from USERS where USERNAME = ?';
+    let stmt = 'select * from Users where USERNAME = ?';
     let data = [req.session.user];
     
-    // let user_info = await query(stmt, data);
-    let user_info = "something";
+    let user_info = await query(stmt, data);
+    // let user_info = "something";
     
-    res.render('account', { user : user_info, username_taken : false, password_match : true });
+    // console.log(user_info[0]);
+    
+    res.render('account', { user : user_info[0], password_match : true });
     
 });
 
 router.post('/', async function(req, res) {
     
-    let stmt = 'select * from USERS where USERNAME = ?';
+    let stmt = 'select * from Users where USERNAME = ?';
     let data = [req.session.user];
     
     let user_info = await query(stmt, data);
     
-    let new_user_info = {"firstname" : req.body.firstname, "lastname" : req.body.lastname, "username" : req.body.username, "bio" : req.body.bio};
+    // console.log(user_info[0], "\n old info" );
     
-    let username_taken = await query(stmt, [new_user_info.username]);
+    let new_user_info = { "firstName" : req.body.firstname, "lastName" : req.body.lastname, "bio" : req.body.bio};
     
-    let u_taken = false;
+    console.log(new_user_info, "\n new info" );
+    
+    // let username_taken = await query(stmt, [new_user_info.username]);
+    
     let p_mismatch = true;
     
-    if(req.body.password != user_info.password || (username_taken.length != 0 && username_taken.username != req.session.user) ) {
+    if(req.body.password != user_info[0].password ) {
         
-        if(username_taken.length != 0 && username_taken.username != req.session.user) {
+        // if(username_taken.length != 0 && username_taken.username != req.session.user) {
             
-            // res.render('account', { user : user_info, username_taken : false, password_match : false });
+        //     // res.render('account', { user : user_info, username_taken : false, password_match : false });
             
-            u_taken = true;
+        //     u_taken = true;
+        //     console.log("username taken\n");
             
-        }
+        // }
         if(req.body.password != user_info.password) {
             
             // res.render('account', { user : new_user_info, username_taken : false, password_match : true });
             
             p_mismatch = false;
+            console.log("password no matchy\n");
     
         }
         
-        res.render('account', { user : new_user_info, username_taken : u_taken, password_match : p_mismatch });
+        res.render('account', { user : new_user_info, password_match : p_mismatch });
         
     }
     else {
         
-        let update_stmt = "update USERS set firstname = ?, lastname = ?, username = ?, bio = ? where USERNAME = ?";
+        let update_stmt = "update Users set firstname = ?, lastname = ?, bio = ? where USERNAME = ?";
         
-        await query(update_stmt, [new_user_info.firstname, new_user_info.lastname, new_user_info.username, new_user_info.bio, req.session.user]);
+        await query(update_stmt, [new_user_info.firstName, new_user_info.lastName, new_user_info.bio, req.session.user]);
         
-        req.session.user = new_user_info.username;
+        console.log("updating user info");
         
+        res.redirect('user/' + req.session.user);
     }
 
 
-    res.redirect('user/' + req.session.user);
     
     
 });
