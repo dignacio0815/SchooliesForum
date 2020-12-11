d381c9564d1db4deabf52402af64cf6317d02a4f
Denize Ignacio <denizeignacio@gmail.com>
2020-12-11 11:41:38 -0800

    Removed console.log statements

--- routes/login.js
+++ routes/login.js
@@ -7,21 +7,9 @@ router.get('/', (req, res) => {
 
 // router.post('/', ())
 router.post('/', async function(req, res, next) {
-    
     let stmt = 'SELECT * FROM Users WHERE username=? and password=?';
-    
-    console.log(req.body.username);
-    
     let data = [req.body.username, req.body.password];
-    
     let userLoggedIn = await query(stmt,data);
-    
-//     console.log(req.body['login'])
-//     let username = req.body['login'][0];
-//     let password = req.body['login'][1];
-//     let userLoggedIn = await validateLogin(username, password);
-
-    console.log(userLoggedIn);
     if(userLoggedIn.length) {
         req.session.authenticated = true;
         req.session.user = req.body.username;
@@ -30,25 +18,6 @@ router.post('/', async function(req, res, next) {
     }
     res.render('login', {invalidLogin:true});
 });
-
-// function validateLogin(username, password) {
-//     const db = dbConnection();
-//     db.connect(function(err) {
-//         if (err) {
-//             console.log('error when connecting to db:', err);
-//         }
-//     });
-//     let stmt = 'SELECT * FROM users WHERE username=? and password=?';
-
-//     let data = [username, password];
-//     return new Promise(function(resolve, reject) {
-//         db.query(stmt, data, function(error, results) {
-//             if (error) throw error;
-//             console.log(results);
-//             resolve(results);
-//         })
-//     });
-// };
 
 function query(stmt, data) {
     return new Promise(function(resolve, reject) {
@@ -59,6 +28,5 @@ function query(stmt, data) {
         });
     });
 }
-
 
 module.exports = router;
